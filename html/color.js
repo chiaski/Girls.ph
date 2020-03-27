9e458107b407273d52361453cbfb8efeb0c5e858
Chia <chiamisola@gmail.com>
2020-03-26 23:13:29 -0400

    add more colors

--- html/color.js
+++ html/color.js
@@ -1,7 +1,7 @@
 
 
 
-styles = [ ["#d94530", "#f2bee2"], ["#272222", "#f4f3ef"], ["#eb9ca1", "#f4f1df"]];
+styles = [ ["#d94530", "#f2bee2"], ["#272222", "#f4f3ef"], ["#eb9ca1", "#f4f1df"], ["#6e9a75", "#f7f2d2"], ["#e888ce", "#d45e09"],  ["#de704f", "#a9c0ed"]];
 
 let choice = styles[Math.floor(Math.random() * styles.length)];
 
