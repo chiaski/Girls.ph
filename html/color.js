9e2b8b4d96886ff3f5459386dff845fe49891930
Chia <chiamisola@gmail.com>
2020-03-26 23:14:54 -0400

    Update color.js

--- html/color.js
+++ html/color.js
@@ -1,7 +1,7 @@
 
 
 
-styles = [ ["#d94530", "#f2bee2"], ["#272222", "#f4f3ef"], ["#eb9ca1", "#f4f1df"], ["#6e9a75", "#f7f2d2"], ["#e888ce", "#d45e09"],  ["#de704f", "#a9c0ed"]];
+styles = [ ["#d94530", "#f2bee2"], ["#272222", "#f4f3ef"], ["#eb9ca1", "#f4f1df"], ["#6e9a75", "#f7f2d2"], ["#e888ce", "#d45e09"], ["#de704f", "#a9c0ed"], ["#dfebed", "#3662ba"], ["#e3e867", "#1d1e21"]];
 
 let choice = styles[Math.floor(Math.random() * styles.length)];
 
